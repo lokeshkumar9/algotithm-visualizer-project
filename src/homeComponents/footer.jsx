1cafa929315265f08eedc9361552ecb8e15ee706
lokesh kumar <94776721+lokeshkumar9@users.noreply.github.com>
2023-07-01 20:05:03 +0530

    Add files via upload

---
+++ src/homeComponents/footer.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import GitHubButton from 'react-github-btn'
+import "./style.css";
+class Footer extends Component {
+    render() {
+        return (
+            <footer class="page-footer font-small special-color-dark pt-4 ">
+
+                <div className='flex-wrap' style={{textAlign:"center"}}>
+                </div>
+
+
+            </footer>
+        );
+    }
+}
+
+export default Footer;
